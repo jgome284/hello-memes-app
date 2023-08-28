62a37a5abef6a2ec51c7809470ca873f68c9b420
Jose M. Gomez <gomezjosem@live.com>
2023-08-27 22:35:19 -0600

    added comments

--- app.py
+++ app.py
@@ -1,25 +1,39 @@
+# import dependencies
 import cowsay
-import time
-
 import redis
+import time
 from flask import Flask
 
+# create app instance
 app = Flask(__name__)
+
+
+# create a connection to the Redis server
 cache = redis.Redis(host='redis', port=6379)
 
+
+# increment the page hits counter that is stored in the redis cache
 def get_hit_count():
+    # specify the max number of connection attempts to the Redis server
     retries = 5
     while True:
         try:
+            # increment the value of the page hits key by 1
             return cache.incr('hits')
+        # in case of a connection error
         except redis.exceptions.ConnectionError as exc:
+            # if there are no more retry attempts
             if retries == 0:
+                # raise the ConnectionError exception
                 raise exc
+            # adjust remaining allowed connection attempts
             retries -= 1
+            # wait before attempting a new connection
             time.sleep(0.5)
 
+# set app route at '/'
 @app.route('/')
+# define view function for route
 def hello():
     count = get_hit_count()
-    input = 'Hello World! I have been seen {} times.\n'.format(count)
-    return input
+    return 'Hello World! I have been seen {} times.\n'.format(count)
